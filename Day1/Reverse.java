90a64b11e383651dbbf21a2650a66c8d3ff5a8ec
bibikashrt <bibikashrestha17@gmail.com>
2026-05-25 17:52:02 +0545

    refactor: add LibraryManager abstraction and improve CRUD logic

--- Day1/Reverse.java
+++
@@ -1,7 +0,0 @@
-public class Reverse {
-    public static void main(String[] args) {
-        String s = "Hello";
-        String rev = new StringBuilder(s).reverse().toString();
-        System.out.println("Reverse: " + rev);
-    }
-}
